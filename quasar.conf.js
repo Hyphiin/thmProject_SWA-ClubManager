28d37060ef8b0402e7b3f85d25bf787b61669e5e
Felix Wegener <felix.wegener@iem.thm.de>
2022-03-08 19:14:16 +0100

    Initialize Firebase

--- quasar.conf.js
+++ quasar.conf.js
@@ -21,7 +21,7 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
-    boot: ["i18n", "axios"],
+    boot: ["firebase", "i18n", "axios"],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ["app.scss"],
@@ -94,9 +94,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [
-        'Dialog'
-      ],
+      plugins: ["Dialog"],
     },
 
     // animations: 'all', // --- includes all animations
---
+++ src/boot/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, onAuthStateChanged, getRedirectResult } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmiTBHT2nkQFLFrQCUQaM1-ro136iMXa8",
+  authDomain: "club-manager-179a2.firebaseapp.com",
+  projectId: "club-manager-179a2",
+  storageBucket: "club-manager-179a2.appspot.com",
+  messagingSenderId: "477064358556",
+  appId: "1:477064358556:web:faf7e110ba93f4b20a5b59",
+  measurementId: "G-DR2RXB0FV1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+onAuthStateChanged(auth, (user) => {
+  /* check status */
+});
+
+export default { db, auth };
