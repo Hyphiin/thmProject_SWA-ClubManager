bd59b1dabc7bf573f9c716d0a1bd6ff183af2814
Felix Wegener <felix.wegener@iem.thm.de>
2022-03-09 19:14:39 +0100

    Added Edit-Function for Field

--- src/pages/field-management/fields-overview.vue
+++ src/pages/field-management/fields-overview.vue
@@ -16,7 +16,7 @@
         </q-img>
 
         <q-list>
-          <q-item clickable to="/fields/edit-field">
+          <q-item clickable @click="getField(el.id)">
             <q-item-section avatar>
               <q-icon color="primary" name="edit" />
             </q-item-section>
@@ -51,6 +51,56 @@
             </q-item-section>
           </q-item>
         </q-list>
+        <q-dialog v-model="editField" persistent>
+          <q-card>
+            <q-card-section class="row items-center">
+              <div class="swa-edit-section">
+                <q-card class="swa-card" flat>
+                  <q-card-section horizontal>
+                    <q-img
+                      src="https://cdn.quasar.dev/img/parallax2.jpg"
+                      class="col"
+                    >
+                      <div class="absolute-bottom text-h6">
+                        {{ name }}
+                      </div>
+                    </q-img>
+
+                    <q-card-actions vertical class="justify-around">
+                      <q-btn flat round color="primary" icon="edit" />
+                      <q-btn flat round color="negative" icon="delete" />
+                    </q-card-actions>
+                  </q-card-section>
+                </q-card>
+
+                <q-toggle
+                  size="lg"
+                  v-model="active"
+                  checked-icon="check"
+                  color="primary"
+                  label="Belegbarer Platz"
+                  unchecked-icon="clear"
+                />
+
+                <q-input outlined v-model="name" label="Name *" />
+
+                <q-input outlined v-model="seat" label="Sitzplätze" />
+                <q-input outlined v-model="standingRoom" label="Stehplätze" />
+              </div>
+            </q-card-section>
+
+            <q-card-actions align="right">
+              <q-btn flat label="Abbrechen" color="primary" v-close-popup />
+              <q-btn
+                flat
+                label="Speichern"
+                color="primary"
+                @click="editFieldFunction(fieldId, name, seat, standingRoom)"
+                v-close-popup
+              />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
       </q-card>
     </q-pull-to-refresh>
   </main-app>
@@ -65,7 +115,14 @@
 
 <script>
 import { defineComponent, ref, onMounted } from "@vue/runtime-core";
-import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "src/boot/firebase";
 import MainApp from "pages/MainApp";
 
@@ -83,7 +140,14 @@ export default defineComponent({
     const alert = ref(false);
     const alertMessage = ref("");
 
+    const editField = ref(false);
+
     const fields = ref([]);
+
+    const fieldId = ref();
+    const name = ref("");
+    const seat = ref(0);
+    const standingRoom = ref(0);
 
     const getFields = async () => {
       fields.value = [];
@@ -95,11 +159,27 @@ export default defineComponent({
       });
     };
 
+    const getField = async (id) => {
+      const field = doc(db, "fields", id);
+
+      const docSnap = await getDoc(field);
+
+      console.log(docSnap);
+
+      editField.value = true;
+
+      fieldId.value = docSnap.id;
+      name.value = docSnap.data().title;
+      seat.value = docSnap.data().seat;
+      standingRoom.value = docSnap.data().standingRoom;
+    };
+
     const confirmDelete = async (id) => {
       await deleteDoc(doc(db, "fields", id))
         .then(() => {
           alert.value = true;
           alertMessage.value = "Plan erfolgreich gelöscht!";
+          getFields();
           setTimeout(() => {
             alert.value = false;
           }, 5000);
@@ -112,7 +192,32 @@ export default defineComponent({
             alert.value = false;
           }, 5000);
         });
-      console.log("Dokument erfolgreich entfernt!");
+    };
+
+    const editFieldFunction = async (id, name, seat, standingRoom) => {
+      const field = doc(db, "fields", id);
+
+      await updateDoc(field, {
+        title: name,
+        seat: seat,
+        standingRoom: standingRoom,
+      })
+        .then(() => {
+          alert.value = true;
+          alertMessage.value = "Plan erfolgreich editiert!";
+          getFields();
+          setTimeout(() => {
+            alert.value = false;
+          }, 5000);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert.value = true;
+          alertMessage.value = "Fehlgeschlagen :(";
+          setTimeout(() => {
+            alert.value = false;
+          }, 5000);
+        });
     };
 
     const refresh = (done) => {
@@ -133,8 +238,15 @@ export default defineComponent({
       addSomething,
       alert,
       alertMessage,
+      editField,
       fields,
+      getField,
+      fieldId,
+      name,
+      seat,
+      standingRoom,
       confirmDelete,
+      editFieldFunction,
       refresh,
     };
   },
